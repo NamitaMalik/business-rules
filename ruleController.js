140c67f02ca9897ee56ec6fc1f222c42089c7318
Amit <amit2.kumar@paytm.com>
2015-02-25 17:39:27 +0530

    Adding tree.

--- ruleController.js
+++ ruleController.js
@@ -1,28 +1,22 @@
 /**
- * Created by namita on 23/2/15.
+ * Created by Namita Malik on 23/2/15.
  */
 var myRuleEngine = angular.module('myRuleEngine', []);
 myRuleEngine.controller('FirstCtrl', ['$scope', function ($scope) {
-    $scope.ruleObject = {
-        ruleList: ['All', 'Any'],
-        rules: []
+  var response = JSON.parse('{"data":{"all":[{"name":"user_id","operator":"present","error":"You need to login before applying voucher code"},{"name":"user_id","operator":"equals","value":12345,"error":"Sorry, the code is not applicable for this user, login with the correct credentials to avail the benefits"},{"name":"promocode_usage_amount","operator":"lessThan","value":50,"error":"Your voucher code balance has exhausted"}]}}');
+  if (response && response.data) {
+    $scope.rules = response.data;
+    $scope.ruleList = ['all', 'any'];
+    $scope.addRules = function (rules, isSubRule) {
+      if (isSubRule) {
+        rules.push({all: [{name: 'A'}]});
+      } else {
+        rules.push({name: "A"});
+      }
+      localStorage.response = JSON.stringify($scope.rules);
     };
-    $scope.addRules = function () {
-        $scope.ruleObject.rules.push({});
+    $scope.removeRule = function (rules, index) {
+      rules.splice(index, 1)
     };
-    $scope.removeRule = function (index) {
-        $scope.ruleObject.rules.splice(index, 1)
-    };
+  }
 }]);
-myRuleEngine.directive('renderRules', function () {
-    return {
-        restrict: "E",
-        templateUrl: "ruleTemplate.html"
-    };
-});
-myRuleEngine.directive('renderSubCondition', function () {
-    return {
-        restrict: "E",
-        templateUrl: "subConditionTemplate.html"
-    };
-});
